32d333e1ff25eb8c6a27f7e1874de46be9417470
Evgenia Afanasieva <electrojanet@gmail.com>
2023-01-29 15:02:11 +0300

    Added folder Task1

---
+++ Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿//  Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+int number = new Random().Next(2, 10);
+int power = new Random().Next(2, 5);
+int result = Power(number, power);
+System.Console.WriteLine($"{number}, {power} -> {result}");
+
+static int Power(int num, int pow)
+{
+    int count = 1;
+    int result = num;
+
+    while(pow > count)
+    {
+        result *= num;
+        count++;
+    }
+
+    return result;
+}
