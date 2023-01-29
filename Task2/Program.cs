8b359b15dc7aca14acd3a5073a14c1b005a18e7b
Evgenia Afanasieva <electrojanet@gmail.com>
2023-01-29 15:03:31 +0300

    Added folder Task2

---
+++ Task2/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает 
+// на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int number = new Random().Next(1, 1000);
+int sum = GetDigitSum(number);
+System.Console.WriteLine($"{number} -> {sum}");
+
+static int GetDigitSum(int number)
+{
+    int sum = 0;
+
+    while(number > 0)
+    {
+        sum = sum + number % 10;
+        number = number / 10;
+    }
+
+    return sum;
+}
